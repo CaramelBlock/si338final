1dce9c39e4857e8b1d9fbba6dcdbbf8ff59201af
Cassidy McKenna <kittycatmckenna@gmail.com>
2025-11-26 19:57:27 -0500

    added drag effect

--- js/bedroom.js
+++ js/bedroom.js
@@ -20,12 +20,41 @@ for (i = 0; i<items.length; i++){
    })
 
 //   creates background #d94ca0 when clicked
-   items[i].addEventListener("click",function(){
+   items[i].addEventListener("click",function(event){
+        zindex = 3 
        
        if (this.style.getPropertyValue("background-color") != "rgb(255, 219, 226)"){
             this.style.setProperty("background-color","#ffdbe2")
             this.style.setProperty("border","pink 3px solid")
             this.style.setProperty("box-shadow", "0px 0px 5px #53838c")
+            posX = event.clientX - this.width/2
+            posY = event.clientY - this.height/2
+            image = document.createElement("img")
+            src = this.getAttribute("src")
+            alt = this.getAttribute("alt")
+            image.setAttribute("alt", alt)
+            image.setAttribute("src", src)
+            image.classList.add("sticker")
+            image.style.setProperty("top",posY+"px")
+            image.style.setProperty("left",posX+"px")
+            document.querySelector(".stickers").appendChild(image)
+            zindex ++
+
+            document.addEventListener("mousemove",function(event){
+                posX = event.clientX - image.width/2
+                posY = event.clientY - image.height/2
+                image.style.setProperty("z-index", 1000)
+                image.style.setProperty("top",posY+"px")
+                image.style.setProperty("left",posX+"px")
+            })
+            document.querySelector(".canvas").addEventListener("click",function(event){
+                furniture = image.cloneNode()
+                image.style.setProperty("display","none")
+                furniture.style.setProperty("z-index", zindex)
+                furniture.style.setProperty("pointer-events","none")
+                document.querySelector(".furnitures").appendChild(furniture)
+                
+            })
         }
         else{
             this.style.setProperty("background-color","inherit")
